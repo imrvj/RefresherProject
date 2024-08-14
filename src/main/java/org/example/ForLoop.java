d8cbaec11005b0b1081c7012cb4464ea679ae53c
RV <sng.rv07@gmail.com>
2024-08-14 21:45:31 +0530

    Updated Loop

--- src/main/java/org/example/ForLoop.java
+++ src/main/java/org/example/ForLoop.java
@@ -3,7 +3,7 @@ package org.example;
 public class ForLoop {
     public static void main(String[] args)
     {
-        for(int i=0;i<=10;i++)
+        for(int i=0;i<=100;i++)
         {
             System.out.println(i);
         }
